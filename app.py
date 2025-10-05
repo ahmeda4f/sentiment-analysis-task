cfb1ae6478cdae1d50a64d2063ebf3cd7b2fba88
Ahmed <127054826+ahmeda4f@users.noreply.github.com>
2025-10-05 17:18:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ def load_model():
     model_name = "Ahmed-Ashraf-00/sentiment-model"
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model.config.id2label = {0: "Positive", 1: "Negative", 2: "Neutral", 3: "Irrelevant"}
+    model.config.label2id = {"Positive": 0, "Negative": 1, "Neutral": 2, "Irrelevant": 3}
     return model, tokenizer
 
 model, tokenizer = load_model()
@@ -24,11 +26,9 @@ if st.button("Analyze"):
             probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
             pred = torch.argmax(probs, dim=1).item()
             confidence = torch.max(probs).item() * 100
-
-        labels = ["Positive", "Negative", "Neutral", "Irrelevant"]
-
+        label = model.config.id2label[pred]
         st.subheader("Result:")
-        st.markdown(f"**Prediction:** {labels[pred]}")
+        st.markdown(f"**Prediction:** {label}")
         st.markdown(f"**Confidence:** {confidence:.2f}%")
         st.progress(confidence / 100)
     else:
